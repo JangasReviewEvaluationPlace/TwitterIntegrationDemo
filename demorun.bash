273772a8be8278746a5634b5e9e4a41fbc8e9fdf
JangasCodingplace <janisgoesser92@gmail.com>
2021-06-13 21:09:55 +0200

    Update submodule

---
+++ demorun.bash
@@ -0,0 +1,31 @@
+docker-compose -f docker-compose.database.yml up -d
+
+docker-compose -f docker-compose.kafka.yml up -d
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:8083/connectors); do
+    echo "Connect not available. Wait 5s - it might take some time"
+    sleep 5
+done
+
+echo "Connect available"
+
+docker-compose -f docker-compose.elastic.yml up -d
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:9200/); do
+    echo "Elastic not available. Wait 5s - it might take some time"
+    sleep 5
+done
+
+echo "Elasticsearch available"
+
+docker-compose -f docker-compose.data-processing.yml up -d
+
+echo "Creating Connectors"
+
+curl -i -X POST http://localhost:8083/connectors \
+  -H "Content-Type: application/json" \
+  -d "$(<'connect/connector_properties/twitter_source.json')"
+
+# curl -i -X POST http://localhost:8083/connectors \
+#   -H "Content-Type: application/json" \
+#   -d "$(<'connect/connector_properties/twitter_elastic_sink.json')"
